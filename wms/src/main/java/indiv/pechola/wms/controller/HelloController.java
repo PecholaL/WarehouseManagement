48bb5389678d1bbc1548c3267fb6a5c10160dbac
PecholaL <pechola.lee@outlook.com>
2024-06-20 11:01:53 +0800

    pechola

--- wms/src/main/java/indiv/pechola/wms/controller/HelloController.java
+++ wms/src/main/java/indiv/pechola/wms/controller/HelloController.java
@@ -5,6 +5,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
+    // Test
     @GetMapping
     public String hello(){
         return "hola";
