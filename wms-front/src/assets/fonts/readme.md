9c8d8d8748d8dcbe9c71806e46ba2de313115bbc
PecholaL <pechola.lee@outlook.com>
2024-06-30 15:15:12 +0800

    pechola

--- wms-front/src/assets/fonts/readme.md
+++ wms-front/src/assets/fonts/readme.md
@@ -2,4 +2,4 @@ Please obtain the fonts from:
 1. [Intel-One-Mono](https://github.com/intel/intel-one-mono)  
 2. [霞鹜文楷](https://github.com/lxgw/LxgwWenKai)  
 
-Rename the files to LXGWWenKai.ttf and intelone.otf  
+Rename the files to *LXGWWenKai.ttf* and *intelone.otf*, respectively.  
--- wms-front/src/components/WmsIndex.vue
+++ wms-front/src/components/WmsIndex.vue
@@ -1,6 +1,6 @@
 <template>
-    <el-container style="height: 500px; border: 1px solid #eee">
-        <el-aside width="200px" style="background-color: rgb(238, 241, 246)">
+    <el-container style="height: 100%; border: 1px solid #eee">
+        <el-aside width="200px" style="background-color: rgb(238, 241, 246); height: 100%;">
             <el-menu :default-openeds="['1', '3']">
                 <el-submenu index="1">
                     <template slot="title"><i class="el-icon-message"></i>导航一</template>
@@ -50,8 +50,8 @@
             </el-menu>
         </el-aside>
 
-        <el-container>
-            <el-header style="text-align: right; font-size: 12px">
+        <el-container style="height: 100%;">
+            <el-header style="text-align: right; font-size: 12px; height: 100%;">
                 <el-dropdown>
                     <i class="el-icon-setting" style="margin-right: 15px"></i>
                     <el-dropdown-menu slot="dropdown">
@@ -63,7 +63,7 @@
                 <span>Pechola</span>
             </el-header>
 
-            <el-main>
+            <el-main style="height: 100%;">
                 <el-table :data="tableData">
                     <el-table-column prop="date" label="日期" width="140">
                     </el-table-column>
@@ -78,15 +78,19 @@
 </template>
 
 <style>
-.el-header {
-    background-color: #B3C0D1;
-    color: #333;
-    line-height: 60px;
-}
+    .el-header {
+        background-color: #daa56e;
+        color: #000000;
+        line-height: 60px;
+    }
 
-.el-aside {
-    color: #333;
-}
+    .el-main{
+        padding: 5px;
+    }
+
+    .el-aside {
+        color: #333;
+    }
 </style>
 
 <script>
@@ -99,7 +103,7 @@ export default {
             address: '上海市普陀区金沙江路 1518 弄'
         };
         return {
-            tableData: Array(10).fill(item)
+            tableData: Array(20).fill(item)
         }
     }
 };
