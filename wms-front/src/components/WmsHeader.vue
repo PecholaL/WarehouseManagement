7c1487df245ec173f625be4f677911f52f883248
PecholaL <pechola.lee@outlook.com>
2024-07-01 15:19:33 +0800

    pechola

---
+++ wms-front/src/components/WmsHeader.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+        <el-dropdown>
+            <i class="el-icon-setting" style="margin-right: 15px"></i>
+            <el-dropdown-menu slot="dropdown">
+                <el-dropdown-item>查看</el-dropdown-item>
+                <el-dropdown-item>新增</el-dropdown-item>
+                <el-dropdown-item>删除</el-dropdown-item>
+            </el-dropdown-menu>
+        </el-dropdown>
+        <span>Pechola</span>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "WmsHeader"
+}
+</script>
+
+<style scoped>
+
+</style>
