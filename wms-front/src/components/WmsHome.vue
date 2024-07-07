661612277c6ef49e840521fc1e2f7eea98e0e947
PecholaL <pechola.lee@outlook.com>
2024-07-07 15:00:07 +0800

    pechola

---
+++ wms-front/src/components/WmsHome.vue
@@ -0,0 +1,19 @@
+<template>
+    <div style="">
+        <h1 style="">#1918 Warehouse M.S. Home</h1>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "WmsHome", 
+    data() {
+        return {
+        }
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
